73359e354236da9cef6d072c27b42aa014612340
Xiaoyan (Adele) <54793414+Xiaoyan-Adele@users.noreply.github.com>
2020-03-30 16:50:55 +0800

    updated item code

--- Code/spiders/items.py
+++ Code/spiders/items.py
@@ -5,12 +5,13 @@ from datetime import datetime
 def convert_date(text):
   return datetime.strptime(text, '%b %d, %Y')
 
-class Automobileitems (Item):
+class Vehiculeitems (Item):
     title = Field()
     link = Field()
     teaser = Field()
     publication = Field(
         input_processor = MapCompose(convert_date)
         output_processor = TakeFirst()
+    article_content =Field()
     )
     tags = Field()
