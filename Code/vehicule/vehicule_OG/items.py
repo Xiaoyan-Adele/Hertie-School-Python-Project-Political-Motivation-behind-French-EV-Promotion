d37b045503c6667d8e1a79b876ab8fbedb9c24f5
Xiaoyan (Adele) <54793414+Xiaoyan-Adele@users.noreply.github.com>
2020-04-07 01:28:51 +0800

    .

--- Code/vehicule/vehicule_OG/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-#to define the fields of my items
-class VehiculeItems(scrapy.Item):
-    title = scrapy.Field()
-    link = scrapy.Field()
-    teaser = scrapy.Field()
-    publication = scrapy.Field()
-    
